6b1ab73b016c555039ca0cd087ac374cc35ed89d
ytw <a9600125a@gmail.com>
2023-04-26 11:04:00 +0800

    feat: add doc

---
+++ doc/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { CryMomButton } from "../src/cry-mom-button";
+
+const App = () => {
+  return (
+    <main id="main">
+      <CryMomButton />
+    </main>
+  );
+};
+
+export default App;
